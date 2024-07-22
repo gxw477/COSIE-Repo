4d9df959d120575d5ff1f4ae8b88ab8025b712d6
gxw477 <george.west@icr.ac.uk>
2024-07-22 12:49:05 +0100

    Added ground truth speckle calculator with attenuation corr
    Allows use of images from different depths with attenuation correction included

--- edgeDetectionMulti.m
+++ edgeDetectionMulti.m
@@ -1,43 +1,86 @@
 
-function [mEdgeSpect ,y0 ] = edgeDetectionMulti(fullIM,fs,yVals,kLengthPER,c0,f0,xBool)
+function [mEdgeSpect ,y0] = edgeDetectionMulti(dir)
+    
+    %kernel length in periods of T0 = 1/f0
+    kLengthPER = 10;
+    xBool = 17:111;
+   
+   
+    IDF = load([dir,'\IDF_FiltAvg.mat']);
 
-    %get fast time axis as the first dimension
-    if size(fullIM,2)>size(fullIM,1)
-        fullIM = fullIM';
+    fnames = ls([dir,'\BFimgData*']);
+    nImages = size(fnames,1);
+
+    mEdgeSpect = zeros(1,nImages);
+    y0 = zeros(1,nImages);
+    
+    for iImage = 1:nImages
+        
+        bfImgData = load([dir,'\BFimgData',num2str(iImage),'.mat']);
+
+        fs = bfImgData.fs;
+        c0 = bfImgData.Resource.Parameters.speedOfSound;
+        yVals = bfImgData.yVals;
+        f0 = bfImgData.Trans.frequency*1e6;
+        fullIM = bfImgData.fullIM;
+        
+        lambda = c0/f0;
+        offset = bfImgData.Receive(1).startDepth*lambda;
+        wSize = IDF.Filt.wsize*lambda; 
+
+        IDF.Filt.yVals = offset + wSize/2 + (IDF.Filt.wpos )./2*lambda/bfImgData.Receive(1).samplesPerWave;
+        
+        [~,nF0_IDF] = min(abs(f0/1e6-IDF.Filt.f));
+
+        %get fast time axis as the first dimension
+        if size(fullIM,2)>size(fullIM,1)
+            fullIM = fullIM';
+        end
+        
+        if 0 
+            figure 
+            iq = rf2iq(fullIM(:,xBool),fs);
+            bm = bmode(iq,50);
+            
+            xDummy = 1:size(iq,1);
+    
+            imagesc(1:size(iq,2),yVals,bm)
+            colormap gray
+            ylim([0 40e-3])
+        end
+
+        kLength_LENGTH = 0.5*kLengthPER*(1/f0)*c0;
+    
+        [~,yIdx]= max(sum(fullIM,2));
+
+        y0_Val = yVals(yIdx);
+        [~,y0IDF_idx] = min(abs(y0_Val-IDF.Filt.yVals));
+
+       
+
+        y0(iImage)= y0_Val;
+     
+        samplesPwavel = 4;
+        kLengthSamples = kLengthPER*samplesPwavel;
+        win = hanning(kLengthSamples);
+    
+        yBool = (yIdx - kLengthSamples/2):(yIdx+kLengthSamples/2-1);
+        
+        edgeRegion = fullIM(yBool,xBool).*win;
+        spectVals = abs(fft(edgeRegion));
+    
+        df = fs/(kLengthSamples-1);
+        fVals = (0:kLengthSamples-1).*df;
+    
+        [~ , nF] = min(abs(fVals-f0));
+        
+      
+        mEdgeSpect(iImage) = mean(spectVals(nF,:))/IDF.Filt.F(nF0_IDF,y0IDF_idx);
+
+        
     end
-
-    figure 
-    iq = rf2iq(fullIM(:,xBool),fs);
-    bm = bmode(iq,50);
     
-    xDummy = 1:size(iq,1);
-
-    imagesc(1:size(iq,2),yVals,bm)
-    colormap gray
-    ylim([0 10e-3])
-    
-    kLength_LENGTH = 0.5*kLengthPER*(1/f0)*c0;
-
-    y0 = input('Interface Depth')
-
-    [ ~ , yIdx] = min(abs(yVals-y0));
-    
-    samplesPwavel = 4;
-    kLengthSamples = kLengthPER*samplesPwavel;
-    win = hanning(kLengthSamples);
-
-    yBool = (yIdx - kLengthSamples/2):(yIdx+kLengthSamples/2-1);
-
-
-    edgeRegion = fullIM(yBool,xBool).*win;
-    spectVals = abs(fft(edgeRegion));
-
-    df = fs/(kLengthSamples-1);
-    fVals = (0:kLengthSamples-1).*df;
-
-    [~ , nF] = min(abs(fVals-f0));
-
-    mEdgeSpect = mean(spectVals(nF,:));
-    
-
+    %save([dir,'\edgeSpectVals.mat'],'mEdgeSpect2','y0')
 end
+
+
---
+++ gTruthSpeckleComputation_L74_adaptive_attCorr.m
@@ -0,0 +1,195 @@
+
+clear 
+close all 
+
+%Image Analysis
+
+topDirMaster = uigetdir;
+
+vsxParams = load([topDirMaster,'/VSXoutput.mat']);
+
+
+%% Define BSC kernel properties
+lambda = vsxParams.lambda;
+
+%dtheta = vsxParams.Angle(2)-vsxParams.Angle(1);
+
+kWidth_BSC_lines = 5;
+kLength_BSC_samples = 120;
+oLap = .80;
+
+lWidthF = lambda*(vsxParams.Trans.ElementPos(2,1)-vsxParams.Trans.ElementPos(1,1)) ;
+
+samplesPerAcq = vsxParams.Receive(1).endSample - vsxParams.Receive(1).startSample + 1;
+
+%already defined, but to remind you
+rVals = lambda.*linspace(vsxParams.Receive(1).startDepth,vsxParams.Receive(1).endDepth,samplesPerAcq)  ;
+
+[~, bscFocIdx] = min(abs(rVals - vsxParams.TX(1).focus*lambda));
+
+
+%% Define coherence kernel properties 
+
+%kernel length (wavels)
+nWavels = 3;
+%kernel length (samples)
+kLength_COH = round(vsxParams.Receive(1).samplesPerWave);
+
+
+
+  
+%% 
+
+
+fNames = ls([topDirMaster,'\BFimgData*']);
+nImages =  size(fNames,1);
+
+clearvars fNames
+
+kIdxs = cell(nImages,1);
+
+zSelect = input('Select depth of interest: ')/1e3;
+[~, zIdx] = min(abs(rVals - zSelect));
+axIdxs = zIdx-round(kLength_BSC_samples/2) : zIdx + round(kLength_BSC_samples/2) -1 ;
+
+
+wOption = input('Window Type \n 1 for rectangular \n 2 for tukey \n 3 for Welch : \n ');
+
+if wOption == 1 
+    win = [0,ones(1,kLength_BSC_samples-2),0];
+    wName = 'Rect';
+elseif wOption == 2
+    win = tukeywin(kLength_BSC_samples,0.1)';
+    wName = 'Tukey';
+elseif wOption == 3
+    wName = 'Welsh';
+end
+
+saveDir = [topDirMaster,'/',wName,'/Z',num2str(round(zSelect*1e3)),'/'];
+
+if ~exist(saveDir)
+    mkdir(saveDir)
+end
+
+
+fs = vsxParams.Trans.frequency*1e6*vsxParams.Receive(1).samplesPerWave;
+
+fVals = (0:length(axIdxs)-1).*fs/(length(axIdxs));
+df = fVals(2)-fVals(1);
+nF = round(vsxParams.Trans.frequency*1e6/df);
+ 
+
+kCount = 1; 
+rayCount= 0; 
+
+
+for iImage = 1:nImages
+
+    iImage
+
+    load([topDirMaster,'\BFimgData',num2str(iImage),'.mat'])
+     
+    bM = bmode(iq,30);
+
+    imagesc(bM'); 
+    colormap gray; 
+
+    pause(0.5)
+
+    lRl = 17; %input('Left ray line: ');
+    rRl = 111;%input('Right ray line: ');
+
+    kIdxs{iImage} = lRl:rRl;
+   
+    if length(axIdxs) ~= kLength_BSC_samples
+        error('check bsc kernel length')
+    end
+
+    %% BSC + Coherence Calc
+
+    bscLines = fullIM(kIdxs{iImage},axIdxs);
+    
+    if wOption == 1 || wOption == 2
+        winMatrix = ones(size(bscLines)).*win;
+        bscLines = bscLines.*winMatrix;
+        spect = fft(bscLines,[],2);
+        
+    elseif wOption == 3
+        h = spectrum.welch;                  % Create a Welch spectral estimator.
+        welchObj = psd(h,bscLines','Fs',fs);
+        spect = welchObj.Data'; % transpose because spectAveraging takes the vector the other way
+    end
+    
+    spectAv = spectAveraging(spect,kWidth_BSC_lines,oLap);
+
+    nKsInAverage = size(spectAv,1);
+
+    avSpectAll(kCount:(kCount+nKsInAverage-1),:) = spectAv;
+
+    
+    for iLine2 = 1:length(kIdxs{iImage})
+        
+       cohAll(iLine2 + rayCount ,:) = CoherenceAnalysisFN(squeeze(channelStack(kIdxs{iImage}(iLine2),axIdxs,:)));
+       spectAll(iLine2 + rayCount, :) = spect(iLine2,:);
+       envAll(iLine2 + rayCount,:) = abs(envelope(bscLines(iLine2,:)));
+
+    end
+    
+    kCount = kCount + nKsInAverage; 
+    rayCount = rayCount + length(kIdxs{iImage});
+
+end
+
+
+powf0 = abs(spectAll(:,nF));
+
+mEnv = mean(envAll,2);
+sEnv = std(envAll,0,2);
+snr = mEnv./sEnv;
+
+[bscSurface,segSurface,EML,pctSeg1,redEML,pctSeg2] = COSIE_adaptiveGrid(snr,powf0);
+
+save([saveDir,'envData_COSIE.mat'],'snr','EML','bscSurface','pctSeg1','pctSeg2','redEML','segSurface')
+
+avCohAll = spectAveraging(cohAll,kWidth_BSC_lines,oLap);
+
+
+cohMubCorr = zeros(1,size(cohAll,2));
+
+input('check save folder ')
+
+saveDir2 = [saveDir,'/COSIEoutput_adaptive'];
+
+if ~exist(saveDir2)
+    mkdir(saveDir2)
+end
+
+
+for sumIdx = 1:size(cohAll,2)
+
+    sumIdx/size(cohAll,2)
+
+    cohSum = sum(cohAll(:,1:sumIdx),2);
+
+    cosieParams.APsize = sumIdx;
+    cosieParams.dTH = sumIdx/0.5e3;
+    cosieParams.GT = (mean(powf0));
+    
+    [bscSurface,segSurface,EML,pctSeg1,redEML,pctSeg2] = COSIE_adaptiveGrid(cohSum,powf0);
+    
+    thVector = sort(cohSum);
+
+    save([saveDir2,'/COSIEoutput',num2str(sumIdx),'.mat'],'EML','bscSurface','pctSeg1','pctSeg2','redEML','segSurface','thVector','powf0','cohSum')
+
+    %plot(EML(1,:),EML(2,:),'.')
+    %close all
+    
+
+    R = corrcoef(cohSum,abs(powf0));
+
+    cohMubCorr(sumIdx) = R(2);
+
+end
+
+
+
